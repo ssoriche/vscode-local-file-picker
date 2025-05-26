9c3116a4fb11bc56f6de9510a7f139784d692dcd
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2025-05-26 17:43:28 -0400

    Initial commit: Local File Picker extension

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Change Log
+
+All notable changes to the "Local File Picker" extension will be documented in this file.
+
+## [0.1.0] - 2024-03-XX
+
+### Added
+
+- Initial release
+- Quick file picker for current directory
+- Support for file and directory navigation
+- Fuzzy search functionality
+- VIM keybinding support (`<space>f l`)
+- Basic test infrastructure
+
+### Changed
+
+- N/A
+
+### Fixed
+
+- N/A
---
+++ test/runTest.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { runTests } = require('@vscode/test-electron');
+
+async function main() {
+  try {
+    // The folder containing the Extension Manifest package.json
+    const extensionDevelopmentPath = path.resolve(__dirname, '../');
+
+    // The path to the extension test script - FIXED: added .js extension
+    const extensionTestsPath = path.resolve(
+      __dirname,
+      './suite/index.js'  // Changed from './suite/extension.test' to './suite/index.js'
+    );
+
+    // Download VS Code, unzip it and run the integration test
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+    });
+  } catch (err) {
+    console.error('Failed to run tests:', err);
+    process.exit(1);
+  }
+}
+
+main();
